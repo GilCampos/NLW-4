b5e216458aa3802b587ad0a5e97b299d1a67452d
Gilma Campos <gilmameister@gmail.com>
2021-02-25 20:00:09 -0300

    aula nlw 01 com sucesso

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+//importando o express
+import express from 'express';
+
+const app= express();
+
+// htttp://localhost:3334/users
+app.get("/", (request, response) => {
+    return response.json({message: "Hello World NLW4"});
+});
+
+//1 param = rota(recurso API)
+//2 param = request, response
+app.post("/", (request, response) => {
+    //receber os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso!"});
+});
+
+//criando o servidor, passando a porta e verificando se executa
+app.listen(3334, () => console.log("Server is running"));
