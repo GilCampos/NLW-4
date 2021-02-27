71f9a3e4500f07a3fb4f2b32b2d3fd04dbcf1ba7
Gilma Campos <gilmameister@gmail.com>
2021-02-27 08:36:53 -0300

    criação da entidade users

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 //importanto o reflect-metadata
 import "reflect-metadata";
-
 //importando o express
 import express from 'express';
 import "./database";
+import { router } from "./routes";
 
 const app= express();
+
+app.use(express.json());
+app.use(router);
+
 
 //criando o servidor, passando a porta e verificando se executa
 app.listen(3334, () => console.log("Server is running"));
