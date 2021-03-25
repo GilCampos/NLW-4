9b0494c8f3ba8e035cd832b9c8cbbce32d303a68
Gilma Campos <gilmameister@gmail.com>
2021-03-25 16:34:49 -0300

    -

--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -1,14 +1,13 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from 'uuid';
 
-
+@Entity("surveys")
 class Survey {
     @PrimaryColumn()
     readonly id: string;   
 
     @Column()
     title: string;
-
     @Column()
     description: string;
 
@@ -22,4 +21,4 @@ class Survey {
     }
 }
 
-export {Survey}
+export { Survey };
