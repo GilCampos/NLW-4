74c4629f2a7dcd473f5e9b6736975c99a8ee75c1
Gilma Campos <gilmameister@gmail.com>
2021-03-28 11:33:56 -0300

    passando responsabilidades para app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,4 @@
-//importanto o reflect-metadata
-import "reflect-metadata";
-//importando o express
-import express from 'express';
-import "./database";
-import { router } from "./routes";
-
-
-const app = express();
-app.use(express.json());
-app.use(router);
-
+import { app } from "./app";
 
 //criando o servidor, passando a porta e verificando se executa
 app.listen(3334, () => console.log("Server is running"));
