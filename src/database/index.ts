91adadf0c2e254720778077f3a25050e70ae9865
Gilma Campos <gilmameister@gmail.com>
2021-03-28 11:31:50 -0300

    atualizando arquivo com variável de ambiente NODE_ENV para testes automatizados

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,20 @@
-import { createConnection } from "typeorm";
+//import { createConnection } from "typeorm";
+//createConnection();
+//26/03/2021
 
-createConnection();
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (): Promise<Connection> => {
+
+    const defaultOptions = await getConnectionOptions();
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database:
+            process.env.NODE_ENV === 'test'
+            ? "./src/database/database.test.sqlite"
+            : defaultOptions.database,
+        })
+    );
+};
+
 
