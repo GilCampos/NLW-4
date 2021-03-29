5be46e88f61ab64b9e293ff614bbc3ba6a47c277
Gilma Campos <gilmameister@gmail.com>
2021-03-29 14:18:47 -0300

    criando arquivo para criar conta de teste  enviar email

---
+++ src/services/SendMailService.ts
@@ -0,0 +1,45 @@
+
+import nodemailer, { Transporter } from 'nodemailer';
+
+class SendMailService {
+
+    private client: Transporter;
+
+    constructor(){
+        //criando conta de teste
+        nodemailer.createTestAccount().then(account => {
+            //da documentação em Ethereal mail
+            // Create a SMTP transporter object
+            let transporter = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass
+                }
+            });
+            this.client = transporter;
+        });
+    }
+   
+    //criando um método execute para enviar email
+    //https://ethereal.email
+    async execute(to: string, subject: string, body: string) {
+        const message = await this.client.sendMail({
+            to,
+            subject,
+            html: body,
+            from: "NPS <noreplay@nps.com.br",
+        });
+
+        console.log('Message sent: %s', message.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message)); 
+    }      
+}
+
+//export { SendMailService };
+//criar a instancia automaticamente que vai ser chamado de 
+//send mail controller
+export default new SendMailService();
