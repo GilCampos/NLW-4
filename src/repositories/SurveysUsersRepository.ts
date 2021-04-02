795b0553764f0ecfea84b660cf979c4fa156d4cd
Gilma Campos <gilmameister@gmail.com>
2021-04-02 16:33:41 -0300

    alterando surveysUsersrepositoty por surveysUsersRepository

--- src/repositories/SurveysUsersRepository.ts
+++ src/repositories/SurveysUsersRepository.ts
@@ -2,8 +2,8 @@ import { Entity, EntityRepository, Repository } from "typeorm";
 import { SurveyUser } from "../models/SurveyUser";
 
 @EntityRepository(SurveyUser)
-class SurveysUsersrepository extends Repository<SurveyUser> {
+class SurveysUsersRepository extends Repository<SurveyUser> {
 
 }
 
-export { SurveysUsersrepository };
+export { SurveysUsersRepository };
