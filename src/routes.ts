e98d54f70f219ecb4d6c041e5c88f3e785170071
Gilma Campos <gilmameister@gmail.com>
2021-04-02 20:08:10 -0300

    adicionanado a rota para NpsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { AnswerController } from './controller/AnswerController';
+import { NpsController } from './controller/NpsController';
 import { SendMailController } from './controller/SendMailController';
 import { SurveysController } from "./controller/SurveysController";
 import { UserController } from './controller/UserController';
@@ -10,14 +11,14 @@ const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
-
 router.post("/sendMail", sendMailController.execute);
-
 router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
 
