66fa41d9b112fdc42f63d867709cb0bf4a191e26
Gilma Campos <gilmameister@gmail.com>
2021-04-02 16:32:03 -0300

    criando a rota para answers

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controller/AnswerController';
 import { SendMailController } from './controller/SendMailController';
 import { SurveysController } from "./controller/SurveysController";
 import { UserController } from './controller/UserController';
@@ -8,12 +9,15 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
 
