c0d16b74bb17ce52983fd4922692069002121680
Gilma Campos <gilmameister@gmail.com>
2021-04-02 21:48:18 -0300

    refatorando validação

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UserRepository";
+import * as yup from 'yup';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    //criando um esquema de validação
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    //verificação do schema
+    /* if(!(await schema.isValid(request.body))) {
+      return response.status(400).json({
+        error: "Validação Falhou!"
+      });
+    } */
+
+    try {
+      await schema.validate(request.body, {abortEarly: false});
+    }catch(err){
+      return response.status(400).json({
+        error: err
+      })
+    }
+
     
     const usersRepository = getCustomRepository(UserRepository);
     //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
